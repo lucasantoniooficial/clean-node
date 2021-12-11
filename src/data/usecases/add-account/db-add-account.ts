8563d1541ad61bc67918ffb593f5d0c208211509
Lucas Antonio <lucas.antonio@ezoom.com.br>
2021-12-11 00:26:54 -0300

    refactor: group all protocols in single file

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,6 +1,4 @@
-import { AccountModel } from '../../../domain/models/account'
-import { AddAccount, AddAccountModel } from '../../../domain/usercases/add-account'
-import { Encrypter } from '../../protocols/encrypter'
+import { Encrypter, AddAccountModel, AddAccount, AccountModel } from './db-add-account-protocols'
 
 export class DbAddAccount implements AddAccount {
   private readonly encrypter: Encrypter
