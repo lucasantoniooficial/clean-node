56ceee930e327196d132bc3ee797a52559d12502
Lucas Antonio <lucas.antonio@ezoom.com.br>
2021-12-14 20:38:40 -0300

    refactor: Change bcrypt call for const

--- src/infra/criptography/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -1,6 +1,6 @@
-import bcrypt from 'bcrypt'
-
 import { Encrypter } from '../../data/protocols/encrypter'
+
+const bcrypt = require('bcrypt')
 
 export class BcryptAdapter implements Encrypter {
   private readonly salt: number
